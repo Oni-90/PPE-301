543e3ae0a82052a5d7d95094f3b8d25049c5d173
Oni-90 <amakoo.ahounda@ipnetinstitute.com>
2023-08-02 13:54:31 +0000

    conflit resolu

--- config/bundles.php
+++ config/bundles.php
@@ -1,5 +1,3 @@
-<?php
-
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
@@ -11,9 +9,6 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
-<<<<<<< HEAD
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
-=======
->>>>>>> origin/master
 ];
